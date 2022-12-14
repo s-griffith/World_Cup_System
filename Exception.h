f88f26fdad624acb63dbea1fad59e97ce6df1c13
Sarah <cmcgriffith@gmail.com>
2022-12-15 01:45:04 +0200

    comments

--- Exception.h
+++ Exception.h
@@ -4,21 +4,40 @@
 #include <exception>
 #include <iostream>
 
+/*
+* Class NodeNotFound : std::exception
+* This class represents an exception thrown when a requested node is not found.
+*/
 class NodeNotFound : public std::exception {
+
     /*
-    * Provide information regarding the error - the node that was searched was not found
+    * Provide information regarding the error - the node that was searched was not found.
+    * @param - none
     * @return - const char* - description of exception
     */
     const char* what() const noexcept override
     {
         return "Node Not Found Error: The following node does not exist in the tree";
     }
+
 };
 
+
+/*
+* Class InvalidID : std::exception
+* This class represents an exception thrown when an ID number given is invalid.
+*/
 class InvalidID : public std::exception {
+
+    /*
+    * Provide information regarding the error - the ID given already exists in the tree.
+    * @param - none
+    * @return - const char* - description of exception
+    */
     const char* what() const noexcept override {
         return "Invalid ID Error: ID already exists in the tree.";
     }
+    
 };
 
 #endif //WORLD_CUP_SYSTEM_EXCEPTION_H
