4033932731d59f978af54e87c84619b968657435
Sarah <cmcgriffith@gmail.com>
2022-12-15 04:20:03 +0200

    organization

--- Node.h
+++ Node.h
@@ -2,31 +2,61 @@
 #define NODE_H
 
 /*
-* Class Tree::Node
-* This class is used to create the separate nodes in the tree
+* Class Node
+* This class is used as a base class for the specific kinds of nodes in the tree.
 */
 template <class T>
 class Node {
 public:
-    //Constructor
-    Node();
 
-    //The copy constructor, the assignment operator and the default destructor
-    Node(const Node&) = default;
-    Node& operator=(const Node& other) = default;
+    /*
+    * Constructor of Node class
+    * @param - none
+    * @return - A new instance of Node
+    */     
+   Node();
+
+    /*
+    * Copy Constructor and Assignment Operator of Node class
+    * world_cup does not allow two of the same player or team (repeating ID's).
+    * Therefore the system does not allow a copy constructor or assignment operator.
+    */    
+    Node(const Node&) = delete;
+    Node& operator=(const Node& other) = delete;
+
+    /*
+    * Destructor of Node class
+    * @param - none
+    * @return - void
+    */
     virtual ~Node() = default;
+
+    /*
+    * Returns the ID of a node
+    * @param - none
+    * @return - ID
+    */
     int getID() const;
+
 protected:
-    //The data that the tree holds
+
+    /*
+     * The internal fields of Node:
+     *   The data of the node of type T
+     *   The height of the subtree of the node
+     *   The balance factor of the node
+     *   The key by which the tree is sorted (id)
+     */
     T m_data;
-    //The height of the subtree of this node
     int m_height;
-    //Balance factor of node
     int m_bf;
-    //The key by which the tree is sorted
     int m_id;
 
-    //Friend classes
+    /*
+     * The following classes are friend classes in order to allow full access to private fields and functions of
+     * Node.
+     * This allows Node to be a mostly protected class, while allowing the system to run smoothly.
+    */    
     template <class K>
     friend class TreeExtraPointer;
 
@@ -37,9 +67,7 @@ protected:
     friend class Tree;
 };
 
-//--------------------------------------------Constructors---------------------------------------------------
 
-//Node Constructor
 template <class T>
 Node<T>::Node() :
     m_height(-1),
@@ -47,11 +75,11 @@ Node<T>::Node() :
     m_id(0)
 {}
 
-//-----------------------------------------------------------------------------------------------------------
 
 template <class T>
 int Node<T>::getID() const {
     return m_id;
 }
+
 
 #endif //NODE_H
